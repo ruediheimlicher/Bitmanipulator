41c5c2ebab0be7808cabf74dae38d45709105c56
Ruedi Heimlicher <r.heimlicher@bluewin.ch>
2012-07-09 21:03:42 +0200

    Hexfeld Wert geändert auf +2

--- rBinHexController.m
+++ rBinHexController.m
@@ -113,7 +113,7 @@ int i;
 
 - (IBAction)HexFeldAktion:(id)sender
 {
-	[TestFeld setIntValue:[TestFeld intValue]+1];
+	[TestFeld setIntValue:[TestFeld intValue]+2];
 	NSString* HexCharString=@"0 1 2 3 4 5 6 7 8 9 a b c d e f A B C D E F";
 	//NSArray* HexArray=[[HexString componentsSeparatedByString:@" "]retain];
 	NSCharacterSet* HexSet=[NSCharacterSet characterSetWithCharactersInString:HexCharString];
