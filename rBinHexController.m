0da1569be87dcbfe8ddfdac19a184ac3e90fe004
Ruedi Heimlicher <r.heimlicher@bluewin.ch>
2012-07-09 21:06:23 +0200

    Intfeld Wert auf +3 gesetzt

--- rBinHexController.m
+++ rBinHexController.m
@@ -86,7 +86,7 @@ int i;
 
 - (IBAction)IntFeldAktion:(id)sender
 {
-   [TestFeld setIntValue:[TestFeld intValue]+20];
+   [TestFeld setIntValue:[TestFeld intValue]+3];
 	NSLog(@"IntFeldAktion: tag: %d string: %@",[sender tag],[sender stringValue]);
 	if ([sender intValue]>0xFF)
 	{
