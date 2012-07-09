ae053667f261bc64f8020b4a1527aea3b9733c2a
Ruedi Heimlicher <r.heimlicher@bluewin.ch>
2012-07-09 21:04:39 +0200

    BinAktion Wert +20

--- rBinHexController.m
+++ rBinHexController.m
@@ -86,7 +86,7 @@ int i;
 
 - (IBAction)IntFeldAktion:(id)sender
 {
-   [TestFeld setIntValue:[TestFeld intValue]+10];
+   [TestFeld setIntValue:[TestFeld intValue]+20];
 	NSLog(@"IntFeldAktion: tag: %d string: %@",[sender tag],[sender stringValue]);
 	if ([sender intValue]>0xFF)
 	{
